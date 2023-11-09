2b455bd4b77b498a035db5cc5f91d6135b97a8f9
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2023-11-09 07:34:14 +0200

    Create VehOutput.java

---
+++ VehOutput.java
@@ -0,0 +1,8 @@
+Driving the car
+Honking the car horn
+Pedaling the bicycle
+Ringing the bicycle bell
+Riding the motorcycle
+Revving the motorcycle engine
+Driving the truck
+Honking the truck horn
