55a9cfaa0d72c5dc3b93091816dad6a7700d5c3f
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2023-11-09 07:19:04 +0200

    Create ConDevOutput.java

---
+++ ConDevOutput.java
@@ -0,0 +1,8 @@
+Smartphone connected to network
+Making a call from smartphone
+Tablet connected to network
+Playing a game on tablet
+Laptop connected to network
+Doing work on laptop
+Desktop computer connected to network
+Running software on desktop computer
